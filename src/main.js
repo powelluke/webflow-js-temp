5498fa9f8ca64966f444605bbedf1b6fc9a93532
Luke Powell <luke@into-studios.com>
2023-07-10 20:56:28 -0500

    Packages setup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,40 @@
-import './styles/style.css'
+import { gsap } from "gsap";
+import barba from "@barba/core";
 
-console.log('Hello')
+console.log("barba works");
+
+// Content Animation Function with Fading Out Letters
+const contentAnimation = function (container) {
+  container.querySelectorAll(".header--tt2").forEach(function (element) {
+    let tl = gsap.timeline({ paused: true });
+    tl.to(element.querySelectorAll(".char"), {
+      opacity: 0,
+      duration: 0.15,
+      ease: "power4.out",
+      stagger: { amount: 0.3, from: "random" },
+    });
+    createScrollTrigger(element, tl);
+  });
+};
+
+barba.init({
+  preventRunning: true,
+  transitions: [
+    {
+      sync: false,
+      beforeLeave(data) {
+        contentAnimation(data.current.container);
+      },
+      leave(data) {
+        return gsap.to(data.current.container, { opacity: 0, duration: 0.5 });
+      },
+      enter(data) {
+        return gsap.fromTo(
+          data.next.container,
+          { opacity: 0 },
+          { opacity: 1, duration: 0.5 }
+        );
+      },
+    },
+  ],
+});
